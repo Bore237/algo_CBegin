2ce3a01da99cfc9621b18411c53a29c41a4117fe
Borel <goudjouborel@gmail.com>
2023-06-26 18:39:34 +0200

    Adjust display

--- main.c
+++ main.c
@@ -5,33 +5,32 @@
 #include "numOprAlgo.h"
 #include "arrayAlgo.h"
 
-
 int main()
 {
     /* */
     printf(" The result of number function \n  \n");
-//    bIsFirtNumber(6);
-//    iFatorialInt(5);
-//    vPowerNumber(2,8);
-//    bIsPowerOfTwo(6);
-//    bNumberIsPerfect(33550336);
-//    bConvDecToBinary(602);
+    //    bIsFirtNumber(6);
+    //    iFatorialInt(5);
+    //    vPowerNumber(2,8);
+    //    bIsPowerOfTwo(6);
+    //    bNumberIsPerfect(33550336);
+    //    bConvDecToBinary(602);
 
-/////*  Array function *//////
+    /////*  Array function *//////
     printf(" The result of array function \n \n");
     // Init the variables
-    int tab[5] = {8,3,3,4,5};
-    char strPalindrone[]="radar";
+    int tab[5] = {8, 3, 3, 4, 5};
+    char strPalindrone[] = "radar";
     char strAnagram1[] = "Migraine";
     char strAnagram2[] = "Imaginer";
     float *pfIntersetion;
-    float A[9]={1, 4, 5, 8, 10, 34, 100};
-    float B[9]={3, 2, 7, 10, 25, 26, 27, 34, 100};
-    char pvIntBuffer[]="CELpUkmy";
+    float A[9] = {1, 4, 5, 8, 10, 34, 100};
+    float B[9] = {3, 2, 7, 10, 25, 26, 27, 34, 100};
+    char pvIntBuffer[] = "CELLULE";
 
-    //Call local function
-    float* resultarray = fSumMeanArray(tab,'i');
-    float* maxmin = fMaxMinArrea(tab, 'i');
+    // Call local function
+    float *resultarray = fSumMeanArray(tab, 'i');
+    float *maxmin = fMaxMinArrea(tab, 'i');
     bIsPalindrome(strPalindrone);
     pcInverseString(strPalindrone);
     bIsAnagram(strAnagram1, strAnagram2);
--- numOprAlgo.c
+++ numOprAlgo.c
@@ -3,10 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//import the local file for this source file
+// import the local file for this source file
 #include "numOprAlgo.h"
-
-
 
 /**
  * @brief check if the number is first
@@ -15,29 +13,36 @@
  * @return true  if the number is first
  * @return false  if is and other number
  */
-bool bIsFirtNumber(unsigned int iTestNumber){
+bool bIsFirtNumber(unsigned int iTestNumber)
+{
     /* init the counter of division without rest */
     int iCountDiv = 0;
 
     /* loop of the all element before ref number */
-    for(int i=1; i<= iTestNumber; i++){
-        if((iTestNumber % i) == 0){
+    for (int i = 1; i <= iTestNumber; i++)
+    {
+        if ((iTestNumber % i) == 0)
+        {
             iCountDiv++;
-        }else if(iCountDiv >3){
+        }
+        else if (iCountDiv > 3)
+        {
             break;
         }
     }
 
     /* display the result of the operation*/
-    if(iCountDiv == 2){
+    if (iCountDiv == 2)
+    {
         printf("This number is the first number \n");
         return true;
-    }else{
+    }
+    else
+    {
         printf("This number is not the first number \n");
         return false;
     }
 }
-
 
 /**
  * @brief caculate the factorial of the word
@@ -45,20 +50,21 @@ bool bIsFirtNumber(unsigned int iTestNumber){
  * @param iTestnumber the number that we to obtains factorial
  * @return  factorial of the input number
  */
-unsigned int iFatorialInt(unsigned int iTestnumber){
+unsigned int iFatorialInt(unsigned int iTestnumber)
+{
     /* this value store the result of operation */
     unsigned int iFacResult = 1;
 
     /* loop for all the before the ref value */
-    for(int data=iTestnumber; data>0; data--){
+    for (int data = iTestnumber; data > 0; data--)
+    {
         iFacResult = iFacResult * data;
     }
 
     /* Display result */
-    printf("The factorial of input number is: %d \n", iFacResult);
+    // printf("The factorial of input number is: %d \n", iFacResult);
     return iFacResult;
 }
-
 
 /**
  * @brief calculate the power of number
@@ -66,38 +72,46 @@ unsigned int iFatorialInt(unsigned int iTestnumber){
  * @param fNumber   is the number that we want to obtains power
  * @param iLevelPow   is the power number of the operation
  */
-float vPowerNumber( float fNumber, int iLevelPow){
+float vPowerNumber(float fNumber, int iLevelPow)
+{
     /* this value store the result of operation */
     float fPowResult = 1;
     bool bStateOpr = true;
 
     /* check the state of power */
-    if(iLevelPow < 0){
+    if (iLevelPow < 0)
+    {
         bStateOpr = false;
         iLevelPow = iLevelPow * -1;
-    }else if (iLevelPow == 0){
+    }
+    else if (iLevelPow == 0)
+    {
         return (float)1;
     }
 
     /* loop and  mutiple the input data with him */
-    for(int i=0; i<iLevelPow; i++){
-        if(bStateOpr){
+    for (int i = 0; i < iLevelPow; i++)
+    {
+        if (bStateOpr)
+        {
             fPowResult = fPowResult * fNumber;
-        }else{
+        }
+        else
+        {
             fPowResult = 1 / (fPowResult * fNumber);
         }
     }
 
     /* reset the power */
-    if(bStateOpr == false){
+    if (bStateOpr == false)
+    {
         iLevelPow = iLevelPow * -1;
     }
 
     /* Display the result */
-    printf("The result of %f ^ %d is: %f \n", fNumber,iLevelPow,fPowResult);
+    printf("The result of %f ^ %d is: %f \n", fNumber, iLevelPow, fPowResult);
     return fPowResult;
 }
-
 
 /**
  * @brief check if  the number is power of two
@@ -106,17 +120,20 @@ float vPowerNumber( float fNumber, int iLevelPow){
  * @return true   if is power of two
  * @return false  if is no power of two
  */
-bool bIsPowerOfTwo(int iNumber){
+bool bIsPowerOfTwo(int iNumber)
+{
     /* Do the modulo of that number and check if equal to zero */
-    if((iNumber % 2) == 0){
+    if ((iNumber % 2) == 0)
+    {
         printf("This number is a power of two \n");
         return true;
-    }else{
+    }
+    else
+    {
         printf(" This number is not a power of two \n");
         return false;
     }
 }
-
 
 /**
  * @brief check if the number is pecfect
@@ -125,28 +142,32 @@ bool bIsPowerOfTwo(int iNumber){
  * @return true  if is a is pecfect number
  * @return false if is not a pecfect number
  */
-bool bNumberIsPerfect(unsigned int  iNumber){
+bool bNumberIsPerfect(unsigned int iNumber)
+{
     /* init the counter of division without rest */
     unsigned int iCountDiv = 0;
 
     /* Do the loop for take all the divisor of input number*/
-    for(unsigned int i=2; i<= iNumber; i++){
-        if((iNumber % i) == 0){
-            iCountDiv += (iNumber/i);
+    for (unsigned int i = 2; i <= iNumber; i++)
+    {
+        if ((iNumber % i) == 0)
+        {
+            iCountDiv += (iNumber / i);
         }
     }
 
     /* Display the result */
-    if(iNumber == iCountDiv){
+    if (iNumber == iCountDiv)
+    {
         printf("This number is the pecfect number \n");
         return true;
-    }else{
+    }
+    else
+    {
         printf("This number is not the pecfect number \n");
         return false;
     }
-
 }
-
 
 /**
  * @brief do the convertion the decimal number and binary number
@@ -155,16 +176,21 @@ bool bNumberIsPerfect(unsigned int  iNumber){
  * @return true if the convertion go well
  * @return false if the convertion fail
  */
-bool bConvDecToBinary(unsigned int iNumber){
-    int size = (int) ((log(iNumber)/ log(2)) +1);
-    bool *bResult = malloc(sizeof(bool[size+1]));
+bool bConvDecToBinary(unsigned int iNumber)
+{
+    int size = (int)((log(iNumber) / log(2)) + 1);
+    bool *bResult = malloc(sizeof(bool[size + 1]));
     unsigned int iCount = 0;
 
     /* loop and do the division of two */
-    while (iNumber > 0){
-        if((iNumber %2) == 0){
+    while (iNumber > 0)
+    {
+        if ((iNumber % 2) == 0)
+        {
             *(bResult + iCount) = 0;
-        }else{
+        }
+        else
+        {
             *(bResult + iCount) = 1;
         }
         iNumber /= 2;
@@ -175,7 +201,8 @@ bool bConvDecToBinary(unsigned int iNumber){
 
     /* display the result of the convertion */
     printf("The convertion of %d is: ", iNumber);
-    for(unsigned int i=0; i<=iCount; i++){
-        printf("%d", *(bResult +(iCount-i)));
+    for (unsigned int i = 0; i <= iCount; i++)
+    {
+        printf("%d", *(bResult + (iCount - i)));
     }
 }
